17ce853b6f9c0c09a7ec6502c1e9d01ec731c4df
rl-86 <rl86@live.se>
2025-06-04 15:14:14 +0200

    Started the project from scratch

---
+++ src/main/java/org/practice/lab2restapispringboot/Lab2RestApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.practice.lab2restapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2RestApiSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2RestApiSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/practice/lab2restapispringboot/Lab2RestApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.practice.lab2restapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2RestApiSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
